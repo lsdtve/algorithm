8193bb05cc3c8ba46425c5d99ea16a31e7fddd8d
배용균 <lsdtve@gmail.com>
2021-05-22 23:05:51 +0900

    1532번 동전 교환

---
+++ Python/1532_동전 교환.py
@@ -0,0 +1,14 @@
+def go():
+    # 금화 적을때
+    #   - 은화 -> 금화
+    #   은화 적을때
+    #       - 동화 -> 은화 -> 금화화    # 동화 적을때 은화 -> 은화
+
+
+    return
+
+
+goal = map(int, input().split())
+coin = map(int, input().split())
+
+
