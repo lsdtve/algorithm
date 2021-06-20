1bade696189f1e353ca05ae985dab51adc835bd4
배용균 <lsdtve@gmail.com>
2021-06-20 17:26:17 +0900

    9252번 LCS 2

---
+++ Python/9252_LCS 2.py
@@ -0,0 +1,18 @@
+a = input()
+b = input()
+dp = [[""]*(len(b)+1) for _ in range(len(a)+1)]
+for i in range(1, len(a)+1):
+    for j in range(1, len(b)+1):
+        if a[i-1] == b[j-1]:
+            dp[i][j] = dp[i-1][j-1] + a[i-1]
+        else:
+            if len(dp[i][j-1]) > len(dp[i-1][j]):
+                dp[i][j] = dp[i][j-1]
+            else:
+                dp[i][j] = dp[i-1][j]
+
+if len(dp[-1][-1]) == 0:
+    print(0)
+else:
+    print(len(dp[-1][-1]))
+    print(dp[-1][-1])
